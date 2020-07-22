70905f66b59031d2bc4a8b8420dc8375fd255eae
fabreu <fremin001@hotmail.com>
2020-07-22 17:29:33 -0400

    My first commit

---
+++ BethanysPieShopHRM.UI/Components/TaskLiskBase.cs
@@ -0,0 +1,27 @@
+﻿using BethanysPieShopHRM.Shared;
+using BethanysPieShopHRM.UI.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM.UI.Components {
+    public class TaskLiskBase : ComponentBase {
+        [Inject]
+        public ITaskDataService taskService { get; set; }
+
+        [Inject]
+        public NavigationManager navManager { get; set; }
+
+        public List<HRTask> Tasks { get; set; } = new List<HRTask>();
+
+        protected override async Task OnInitializedAsync() {
+            Tasks = (await taskService.GetAllTasks()).ToList();
+        }
+
+        public void AddTask() {
+            navManager.NavigateTo("taskedit");
+        }
+    }
+}
